b3a09966f92ac83b90c6750d6554ce443b321585
FuzzyWisp <kirill.zagrebnev@yandex.ru>
2020-05-07 21:19:58 +0300

    added like

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './styles/main.scss'
 import './toxin-blocks/range-slider/range-slider'
 import './toxin-blocks/dropdown/dropdown'
 import './toxin-blocks/checkbox-list/checkbox-list'
+import './toxin-blocks/like-button/like-button'
 //import '@src/static/img/logo.svg'
 //import '@src/styles/test.scss'
 
